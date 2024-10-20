e70f6e93d414883d2bea3bcad7cca970556e8307
Akansha <69010429+akansha3550@users.noreply.github.com>
2024-10-20 20:57:22 +0200

    Update updatefrontendnew.sh

--- Automations/updatefrontendnew.sh
+++ Automations/updatefrontendnew.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# Set the Instance ID and path to the .env file
-INSTANCE_ID="i-030da7d31a1dbbffc"
+# Set the VM Name and Resource Group for Azure VM
+VM_NAME="jenkins-server"           # Replace with your Azure VM name
+RESOURCE_GROUP="myrg"              # Replace with your Resource Group
 
-# Retrieve the public IP address of the specified EC2 instance
-ipv4_address=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+# Retrieve the public IP address of the specified Azure VM
+ipv4_address=$(az vm list-ip-addresses --name $VM_NAME --resource-group $RESOURCE_GROUP --query '[0].virtualMachine.network.publicIpAddresses[0].ipAddress' --output tsv)
 
 # Path to the .env file
-file_to_find="../frontend/.env.docker"
+file_to_find="../frontend/.env.docker"   # Replace with the correct path to your .env file
 
 # Check the current VITE_API_PATH in the .env file
 current_url=$(cat $file_to_find)
@@ -16,7 +17,10 @@ current_url=$(cat $file_to_find)
 if [[ "$current_url" != "VITE_API_PATH=\"http://${ipv4_address}:31100\"" ]]; then
     if [ -f $file_to_find ]; then
         sed -i -e "s|VITE_API_PATH.*|VITE_API_PATH=\"http://${ipv4_address}:31100\"|g" $file_to_find
+        echo "Updated VITE_API_PATH to http://${ipv4_address}:31100"
     else
-        echo "ERROR: File not found."
+        echo "ERROR: .env.docker file not found."
     fi
+else
+    echo "No changes needed. VITE_API_PATH is already up-to-date."
 fi
